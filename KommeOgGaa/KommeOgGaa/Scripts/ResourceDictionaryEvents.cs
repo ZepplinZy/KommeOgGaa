7bbb627a724b75e1a953935f2dd9afe5b97e0695
ZepplinZy <kim.danborg@hotmail.com>
2018-05-28 09:43:10 +0200

    commit

---
+++ KommeOgGaa/KommeOgGaa/Scripts/ResourceDictionaryEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace KommeOgGaa.Scripts
+{
+    public partial class ResourceDictionaryEvents : ResourceDictionary
+    {
+
+
+
+        public ResourceDictionaryEvents()
+        {
+            InitializeComponent();
+        }
+
+        void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if ((sender as PasswordBox).Password == "")
+                (sender as PasswordBox).Tag = "Show";
+            else
+                (sender as PasswordBox).Tag = "Hidden";
+        }
+
+
+    }
+}
